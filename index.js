06c75f74148d820375636392b67b9ecf2506b1d4
Wojciech <szmelczerczykwojciech@gmail.com>
2022-01-20 17:50:25 +0100

    cosmetics

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const express = require("express");
 
 const app = express();
 
-const port = process.env.PORT || 3000;
-
 app.get("/", (req, res) => {
   res.send("test");
 });
+
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log("Server is listening...");
